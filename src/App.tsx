6f6c7c9c4c1af5fa108208a1503c5bc55e397bb5
ankkun13 <nguyenan13805@gmail.com>
2025-10-28 17:23:10 +0700

    thêm tính năng

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ImageUpload from "./components/ImageUpload";
+import Result from './components/Result';
+
 
 export default function App() {
   const handleUpload = (file: File | null) => {
@@ -8,8 +11,21 @@ export default function App() {
   };
 
   return (
-    <>
-      <ImageUpload onUpload={handleUpload} />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<ImageUpload onUpload={handleUpload} />} />
+        <Route
+          path="/result"
+          element={
+            <Result
+              diagnosis="Nghi ngờ viêm phổi"
+              probability={0.85}
+              originalImage=""
+              gradcamImage=""
+            />
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
