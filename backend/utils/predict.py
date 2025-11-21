d80f7c552bda59b96f70967378032c0b731223b0
ankkun13 <nguyenan13805@gmail.com>
2025-11-21 11:45:13 +0700

    thay đổi predict

--- backend/utils/predict.py
+++ backend/utils/predict.py
@@ -2,14 +2,20 @@
 import torch.nn.functional as F
 import torch
 
-def predict(model, image_tensor):
+def predict(model, image_tensor, device):
     """
     Thực hiện dự đoán với ảnh đã xử lý.
     Trả về nhãn và xác suất.
     """
+    image_tensor = image_tensor.to(device)
     with torch.no_grad():
         logits = model(image_tensor)
         probs = F.softmax(logits, dim=1)[0].cpu().numpy()
-        cls_idx = int(probs.argmax())
-        label = "PNEUMONIA" if cls_idx == 1 else "NORMAL"
-        return label, float(probs[cls_idx])
+        confidence, predicted_class = torch.max(probs, 1)
+            
+        # Lấy giá trị
+        idx = predicted_class.item()
+        score = confidence.item() * 100 # Đổi ra phần trăm
+        label = ["Viêm phổi" if idx == 1 else "Bình thường"][0]
+
+        return label, score
